3a52e3fb0679c533fb00e7de0e91769174b36c5f
Winson Wong <winson0228@gmail.com>
2018-10-21 08:32:42 -0700

    Action creator for new any vals from firebase

--- confession/src/Actions/Actions.js
+++ confession/src/Actions/Actions.js
@@ -1,20 +1,11 @@
 import {ref} from '../base'
-import {fetchPost} from "./Actions";
 
-const POST_FETCH = "POST_FETCH";
-
-function requestPosts(subreddit) {
-    return {
-        type: POST_FETCH,
-        payload: ''
-    }
-}
+export const POST_FETCH = 'POST_FETCH';
 
 export function fetchPost() {
     // get posts from database using ref
     // return array
     return dispatch => {
-        dispatch(requestPosts())
         ref.on('value', snapshot => {
             dispatch({
                 type: POST_FETCH,
