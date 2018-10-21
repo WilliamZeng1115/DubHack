e3eb7f22adbad36c39e64f26f58000f43c421936
Stanley <stanleyye1996@gmail.com>
2018-10-21 06:47:28 -0700

    b

--- confession/src/Reducers/ReducerManager.js
+++ confession/src/Reducers/ReducerManager.js
@@ -1,9 +1,18 @@
 import {combineReducers} from 'redux';
 import posts from './Posts';
 import activePosts from './ActivePosts';
+import {ref} from "../base";
 
 const allReducer = combineReducers({
-    posts: posts,
+    posts: new Promise(function(resolve, reject) {
+        ref.on('value', function (data) {
+            console.log(data.val());
+            resolve(data.val());
+        });
+    }).then(function(val) {
+        console.log(val);
+        return val;
+    }),
     active_post: activePosts,
 });
 
