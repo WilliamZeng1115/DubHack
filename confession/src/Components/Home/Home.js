8111624d6be0774a6af48f528dc6b1ee42ccf267
Winson Wong <winson0228@gmail.com>
2018-10-20 18:32:13 -0700

    Added basic home page posting functionality

--- confession/src/Components/Home/Home.js
+++ confession/src/Components/Home/Home.js
@@ -4,7 +4,14 @@ class Home extends Component {
 
     render() {
         return (
-            <h2>This is the home page</h2>
+            <div>
+                <h2>Make a confession!</h2>
+
+                <input type="post" name="post"/>
+                <div className = "align-right">
+                    <button type = "submit" > Submit </button>
+                </div>
+            </div>
         )
     }
 }
