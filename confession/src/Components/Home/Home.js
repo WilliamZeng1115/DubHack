15cfab81eda83a745468f1eba3dad38bee9d4fd4
Winson Wong <winson0228@gmail.com>
2018-10-21 00:59:51 -0700

    fixed files

--- confession/src/Components/Home/Home.js
+++ confession/src/Components/Home/Home.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react'
-<<<<<<< Updated upstream
-import './Home.css'
-=======
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 //import {ref} from "../../base";
 //import {post} from "../../Actions/GetDetails";
 
->>>>>>> Stashed changes
 
 class Home extends Component {
 
@@ -18,25 +14,15 @@ class Home extends Component {
                 Your Location:<br/>
                 <input type="location" names="location" className="validate" placeholder={"City"}/>
                 <p></p>
-<<<<<<< Updated upstream
-
                 Your Confession: <br/>
                 <textarea name="home" cols="10" rows="5" placeholder={"Type in your message..."}></textarea>
-                
-=======
                 <input type="post" name="post" placeholder={"Type in your message..."}/>
-
->>>>>>> Stashed changes
                 <div className = "align-right">
                     <p>
                     <button type = "submit"> Submit </button>
                     </p>
                 </div>
-<<<<<<< Updated upstream
-
-=======
                 {this.props.posts}
->>>>>>> Stashed changes
             </div>
         )
     }
